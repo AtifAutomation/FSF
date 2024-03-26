8a76595a696ca6460f4dbd5ec5a34b48b052b997
Atif <atifhossain99@gmail.com>
2024-03-26 20:16:34 +0530

    Atif/Class and Object Concept

---
+++ src/main/java/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    String Name;
+
+    int age;
+
+    public void play()
+    {
+        System.out.println("Student is playing");
+
+    }
+    public void eat()
+    {
+        System.out.println("Student is eating");
+
+    }
+    public void task()
+    {
+        System.out.println("Student is doing some activity");
+
+    }
+}
---
+++ src/main/java/Teacher.java
@@ -0,0 +1,11 @@
+public class Teacher {
+
+    String TeacherName;
+
+    String SubjectName;
+
+    public void teach()
+    {
+        System.out.println("Teaches all subject");
+    }
+}
