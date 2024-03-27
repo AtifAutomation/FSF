74e675413f99651e978352e1d62933b8205ac81f
Atif <atifhossain99@gmail.com>
2024-03-27 18:45:35 +0530

    day3/method/main method concept

---
+++ src/main/java/day3Concept.java
@@ -0,0 +1,12 @@
+public interface day3Concept {
+    public static void main(String[] args) {
+        System.out.println("My name is Atif Hossain");
+        sum(5,6);
+    }
+    public static void sum(int a, int b)
+    {
+        int c=a+b;
+        System.out.println("Sum is :"+c);
+    }
+
+}
