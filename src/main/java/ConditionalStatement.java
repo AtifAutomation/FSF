d49d16aeaa82c8bbd393d4371c5693a1754f12df
Atif <atifhossain99@gmail.com>
2024-03-29 20:35:39 +0530

    conditional statement

---
+++ src/main/java/ConditionalStatement.java
@@ -0,0 +1,37 @@
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        ConditionalStatement c1=new ConditionalStatement();
+       String res=c1.determineCategory(4,"Male");
+        System.out.println(res);
+
+    }
+
+    public String determineCategory(int age,String gender)
+    {
+        String result=null;
+        if((age>0) && (age <=18) && (gender=="Male"))
+        {
+            result="boy";
+        }
+        else if((age>18) && (age <=60) && (gender=="Male"))
+        {
+            result="man";
+        }
+        else if((age>60) && (gender=="Male") ||(gender=="Female") )
+        {
+            result="sc";
+        }
+        else if((age>0) && (age <=18) && (gender=="Female"))
+        {
+            result="girl";
+        }
+        else if((age>=18) && (age <=60) && (gender=="Female"))
+        {
+            result="woman";
+        }
+        else {
+            result="Enter correct age and gender";
+        }
+        return result;
+    }
+}
