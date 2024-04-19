784eed6e43244a0ce804b26608e8617653275c51
Atif <atifhossain99@gmail.com>
2024-04-20 04:31:41 +0530

    day14

---
+++ src/main/java/Browser.java
@@ -0,0 +1,7 @@
+public class Browser {
+
+    public void browserLogic()
+    {
+        System.out.println("Browser : I belong to browser class");
+    }
+}
---
+++ src/main/java/Chrome.java
@@ -0,0 +1,10 @@
+public class Chrome extends Browser{
+    public void browserLogic()
+    {
+        System.out.println("Chrome : I belong to browser class");
+    }
+    public void chromeLogic()
+    {
+        System.out.println("Chrome : I belong to chrome class");
+    }
+}
